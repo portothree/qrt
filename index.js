d8e3e3b143c29bd3ed8a7e84ccfee6031e807e52
Gustavo Porto <gporto@quanticosolutions.com>
2020-10-28 11:16:46 +0000

    :sparkles: add windDirectionMean method

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 'use strict';
+const { deg2rad, rad2deg } = require('./utils');
 
-module.exports = (input, {postfix = 'rainbows'} = {}) => {
-	if (typeof input !== 'string') {
-		throw new TypeError(`Expected a string, got ${typeof input}`);
-	}
+module.exports = {
+	/**
+	 * @param {array} directions
+	 * @return {number}
+	 *
+	 * @example
+	 *
+	 *	avgWind([180, 345, 90])
+	 */
+	windDirectionMean: input => {
+		if (!Array.isArray(input)) {
+			throw new TypeError(`Expected a array, got ${typeof input}`);
+		}
 
-	return `${input} & ${postfix}`;
+		let sinSum = 0;
+		let cosSum = 0;
+
+		input.forEach(direction => {
+			sinSum += Math.sin(deg2rad(direction));
+			cosSum += Math.cos(deg2rad(direction));
+		});
+
+		return (rad2deg(Math.atan2(sinSum, cosSum)) + 360) % 360;
+	},
 };
