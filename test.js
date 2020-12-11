062c7818d0e2443e76869b5bb866dc92781a8c71
Gustavo Porto <gporto@quanticosolutions.com>
2020-12-11 15:24:28 +0000

    test: add more cases to AQI calc function test

--- test.js
+++ test.js
@@ -32,7 +32,10 @@ describe('Air Quality Toolkit', () => {
 			const [PM10, PM25, NO2, O3, SO2] = opts.values;
 			const result = airQualityIndex(PM10, PM25, NO2, O3, SO2);
 
-			expect(result).toEqual({ pollutant, aqi: Number(aqi) });
+			expect(result).toEqual({
+				AQI_POL: pollutant,
+				AQI_GLOBAL: Number(aqi),
+			});
 		},
 		[
 			{ name: 'PM10:5', values: [1, 0, 0, 0, 0] },
@@ -44,6 +47,33 @@ describe('Air Quality Toolkit', () => {
 			{ name: 'PM10:2', values: [52, 0, 0, 0, 0] },
 			{ name: 'PM10:2', values: [100, 0, 0, 0, 0] },
 			{ name: 'PM10:1', values: [102, 0, 0, 0, 0] },
+			{ name: 'PM10:5', values: [0, 1, 0, 0, 0] },
+			{ name: 'PM10:5', values: [0, 10, 0, 0, 0] },
+			{ name: 'PM2.5:4', values: [0, 12, 0, 0, 0] },
+			{ name: 'PM2.5:4', values: [0, 20, 0, 0, 0] },
+			{ name: 'PM2.5:3', values: [0, 22, 0, 0, 0] },
+			{ name: 'PM2.5:3', values: [0, 25, 0, 0, 0] },
+			{ name: 'PM2.5:2', values: [0, 27, 0, 0, 0] },
+			{ name: 'PM2.5:2', values: [0, 50, 0, 0, 0] },
+			{ name: 'PM2.5:1', values: [0, 52, 0, 0, 0] },
+			{ name: 'PM10:5', values: [0, 0, 1, 0, 0] },
+			{ name: 'PM10:5', values: [0, 0, 39, 0, 0] },
+			{ name: 'NO2:4', values: [0, 0, 42, 0, 0] },
+			{ name: 'NO2:4', values: [0, 0, 100, 0, 0] },
+			{ name: 'NO2:3', values: [0, 0, 102, 0, 0] },
+			{ name: 'NO2:3', values: [0, 0, 200, 0, 0] },
+			{ name: 'NO2:2', values: [0, 0, 202, 0, 0] },
+			{ name: 'NO2:2', values: [0, 0, 400, 0, 0] },
+			{ name: 'NO2:1', values: [0, 0, 402, 0, 0] },
+			{ name: 'PM10:5', values: [0, 0, 0, 0, 1] },
+			{ name: 'PM10:5', values: [0, 0, 0, 0, 99] },
+			{ name: 'SO2:4', values: [0, 0, 0, 0, 102] },
+			{ name: 'SO2:4', values: [0, 0, 0, 0, 200] },
+			{ name: 'SO2:3', values: [0, 0, 0, 0, 202] },
+			{ name: 'SO2:3', values: [0, 0, 0, 0, 350] },
+			{ name: 'SO2:2', values: [0, 0, 0, 0, 352] },
+			{ name: 'SO2:2', values: [0, 0, 0, 0, 500] },
+			{ name: 'SO2:1', values: [0, 0, 0, 0, 502] },
 		]
 	);
 });
